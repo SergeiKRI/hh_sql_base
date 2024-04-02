016e88972f739e21c74283b1498981c4ed7ca28c
SerKRI <SK@main.ru>
2024-04-02 18:22:47 +0400

    config database

--- src/manager_base.py
+++ src/manager_base.py
@@ -1,8 +1,25 @@
+from configparser import ConfigParser
+
 
 class DBManager:
-    '''
+    """
     Класс, который будет подключаться к БД PostgreSQL
-    '''
+    """
+    def config(filename="database.ini", section="postgresql"):
+        # create a parser
+        parser = ConfigParser()
+        # read config file
+        parser.read(filename)
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception(
+                'Section {0} is not found in the {1} file.'.format(section, filename))
+        return db
+
     def get_companies_and_vacancies_count(self):
        """
         — получает список всех компаний и количество вакансий у каждой компании.
